e3e96a2f766ddd00cd66d1598a13438179c697fa
Kristin Baumann <hello@kristin-baumann.com>
2025-09-29 12:14:01 +0200

    Added months and period selection in comparison chart

--- js/renderComparisonElements.js
+++ js/renderComparisonElements.js
@@ -10,7 +10,7 @@ import {
   useState,
   useEffect,
 } from "./utils/preact-htm.js";
-import { valueFormatting } from "./helpers.js";
+import { getDateInUTC } from "./helpers.js";
 
 export function renderComparisonElements() {
   console.log("Rendering comparison elements");
@@ -193,6 +193,93 @@ function renderPeriodButtons() {
   }
 }
 
+const periods = [
+  {
+    value: "all",
+    title: "Show all",
+    subtitle: "October to March",
+    icon: "",
+    startPast: {
+      day: "30",
+      month: "9",
+      year: "2024",
+      week: "40",
+      full: "2024-09-30",
+    },
+    endPast: {
+      day: "31",
+      month: "3",
+      year: "2025",
+      week: "14",
+      full: "2025-03-31",
+    },
+    startCurrent: {
+      day: "29",
+      month: "9",
+      year: "2025",
+      week: "40",
+      full: "2025-09-29",
+    },
+    endCurrent: {
+      day: "30",
+      month: "3",
+      year: "2026",
+      week: "14",
+      full: "2026-03-30",
+    },
+  },
+  {
+    value: "pre-holiday",
+    title: "Pre-holiday",
+    subtitle: "October to late November",
+    icon: "",
+    startPast: {
+      day: "30",
+      month: "9",
+      year: "2024",
+      week: "40",
+      full: "2024-09-30",
+    },
+    endPast: {
+      day: "25",
+      month: "11",
+      year: "2024",
+      week: "48",
+      full: "2024-11-25",
+    },
+    startCurrent: {
+      day: "29",
+      month: "9",
+      year: "2025",
+      week: "40",
+      full: "2025-09-29",
+    },
+    endCurrent: {
+      day: "24",
+      month: "11",
+      year: "2025",
+      week: "48",
+      full: "2025-11-24",
+    },
+  },
+  {
+    value: "peak-season",
+    title: "Peak season",
+    subtitle: "Late November to December 25",
+    icon: "",
+    startWeek: "48",
+    endWeek: "51",
+  },
+  {
+    value: "post-holiday",
+    title: "Post-holiday Q5",
+    subtitle: "December 26 to March",
+    icon: "",
+    startWeek: "52",
+    endWeek: "14",
+  },
+];
+
 function ComparisonPeriodButtons() {
   const [selectedPeriod, setSelectedPeriod] = useState("pre-holiday");
 
@@ -206,34 +293,7 @@ function ComparisonPeriodButtons() {
     );
   };
 
-  const buttonContent = [
-    {
-      value: "all",
-      title: "Show all",
-      subtitle: "October to March",
-      icon: "",
-    },
-    {
-      value: "pre-holiday",
-      title: "Pre-holiday",
-      subtitle: "October to late November",
-      icon: "",
-    },
-    {
-      value: "peak-season",
-      title: "Peak season",
-      subtitle: "Late November to December 25",
-      icon: "",
-    },
-    {
-      value: "post-holiday",
-      title: "Post-holiday Q5",
-      subtitle: "December 26 to March",
-      icon: "",
-    },
-  ];
-
-  const buttons = buttonContent.map((btn) => {
+  const buttons = periods.map((btn) => {
     return html`<div
       class="vis-period-button ${selectedPeriod === btn.value
         ? "selected"
@@ -267,6 +327,43 @@ function renderComparisonChart(data) {
   }
 }
 
+// time scales
+function getTimeScale(year = "past", period) {
+  // year can be "past" or "current"
+  // period can be "all", "pre-holiday", "peak-season", "post-holiday"
+
+  const periodObj = periods.find((p) => p.value === period);
+  let startY = null;
+  let startMonth = null;
+  let startDay = null;
+  let endY = null;
+  let endMonth = null;
+  let endDay = null;
+  if (periodObj) {
+    if (year === "past") {
+      startY = periodObj.startPast?.year || null;
+      startMonth = periodObj.startPast?.month || null;
+      startDay = periodObj.startPast?.day || null;
+      endY = periodObj.endPast?.year || null;
+      endMonth = periodObj.endPast?.month || null;
+      endDay = periodObj.endPast?.day || null;
+    } else {
+      startY = periodObj.startCurrent?.year || null;
+      startMonth = periodObj.startCurrent?.month || null;
+      startDay = periodObj.startCurrent?.day || null;
+      endY = periodObj.endCurrent?.year || null;
+      endMonth = periodObj.endCurrent?.month || null;
+      endDay = periodObj.endCurrent?.day || null;
+    }
+  }
+
+  const start = Date.UTC(startY, startMonth - 1, startDay);
+  const end = Date.UTC(endY, endMonth - 1, endDay);
+  console.log("getTimeScale:", { year, period, start, end });
+
+  return d3.scaleLinear().domain([start, end]);
+}
+
 function ComparisonChart({ data }) {
   const [system, setSystem] = useState(
     getDropdownValue("vis-comparison-dropdown-systems")
@@ -297,8 +394,6 @@ function ComparisonChart({ data }) {
   useEffect(() => {
     setChartData(filterData(data));
   }, [system, country, category, vertical]);
-
-  // TODO: add listeners for year, period, userMetric, advertiserMetric
 
   // listen to change in comparison system dropdown
   useEffect(() => {
@@ -333,8 +428,7 @@ function ComparisonChart({ data }) {
   // listen to change in general category
   useEffect(() => {
     const handleCategoryChange = (e) => {
-      const selectedCategory = e.detail.selectedCategory;
-      setCategory(selectedCategory);
+      setCategory(e.detail.selectedCategory);
     };
     document.addEventListener(
       "vis-vertical-filter-category-changed",
@@ -351,8 +445,7 @@ function ComparisonChart({ data }) {
   // listen to change in general vertical
   useEffect(() => {
     const handleVerticalChange = (e) => {
-      const selectedVertical = e.detail.selectedVertical;
-      setVertical(selectedVertical);
+      setVertical(e.detail.selectedVertical);
     };
     document.addEventListener(
       "vis-vertical-filter-vertical-changed",
@@ -362,6 +455,64 @@ function ComparisonChart({ data }) {
       document.removeEventListener(
         "vis-vertical-filter-vertical-changed",
         handleVerticalChange
+      );
+    };
+  }, []);
+
+  // listen to change in year selection
+  useEffect(() => {
+    const handleYearChange = (e) => setYear(e.detail.selectedYear);
+    document.addEventListener("vis-comparison-year-changed", handleYearChange);
+    return () => {
+      document.removeEventListener(
+        "vis-comparison-year-changed",
+        handleYearChange
+      );
+    };
+  }, []);
+
+  // listen to change in period selection
+  useEffect(() => {
+    const handlePeriodChange = (e) => setPeriod(e.detail.selectedPeriod);
+    document.addEventListener(
+      "vis-comparison-period-changed",
+      handlePeriodChange
+    );
+    return () => {
+      document.removeEventListener(
+        "vis-comparison-period-changed",
+        handlePeriodChange
+      );
+    };
+  }, []);
+
+  // listen to change in user metric selection
+  useEffect(() => {
+    const handleUserMetricChange = (e) =>
+      setUserMetric(e.detail.selectedMetric);
+    document.addEventListener(
+      "vis-comparison-user-metrics-changed",
+      handleUserMetricChange
+    );
+    return () => {
+      document.removeEventListener(
+        "vis-comparison-user-metrics-changed",
+        handleUserMetricChange
+      );
+    };
+  }, []);
+  // listen to change in advertiser metric selection
+  useEffect(() => {
+    const handleAdvertiserMetricChange = (e) =>
+      setAdvertiserMetric(e.detail.selectedMetric);
+    document.addEventListener(
+      "vis-comparison-advertiser-metrics-changed",
+      handleAdvertiserMetricChange
+    );
+    return () => {
+      document.removeEventListener(
+        "vis-comparison-advertiser-metrics-changed",
+        handleAdvertiserMetricChange
       );
     };
   }, []);
@@ -384,14 +535,85 @@ function ComparisonChart({ data }) {
   const width =
     visContainer && visContainer.offsetWidth ? visContainer.offsetWidth : 600;
   const height = 600;
-  const margin = { top: 20, right: 1, bottom: 20, left: 1 };
+  const margin = { top: 50, right: 1, bottom: 60, left: 1 };
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
+
+  const months = [
+    {
+      name: "October",
+      begin: "2024-10-01",
+      end: "2024-10-31",
+      year: "past",
+    },
+    { name: "November", begin: "2024-11-01", end: "2024-11-30", year: "past" },
+    { name: "December", begin: "2024-12-01", end: "2024-12-31", year: "past" },
+    { name: "January", begin: "2025-01-01", end: "2025-01-31", year: "past" },
+    { name: "February", begin: "2025-02-01", end: "2025-02-28", year: "past" },
+    { name: "March", begin: "2025-03-01", end: "2025-03-31", year: "past" },
+    {
+      name: "October",
+      begin: "2025-10-01",
+      end: "2025-10-31",
+      year: "current",
+    },
+    {
+      name: "November",
+      begin: "2025-11-01",
+      end: "2025-11-30",
+      year: "current",
+    },
+    {
+      name: "December",
+      begin: "2025-12-01",
+      end: "2025-12-31",
+      year: "current",
+    },
+    {
+      name: "January",
+      begin: "2026-01-01",
+      end: "2026-01-31",
+      year: "current",
+    },
+    {
+      name: "February",
+      begin: "2026-02-01",
+      end: "2026-02-28",
+      year: "current",
+    },
+    { name: "March", begin: "2026-03-01", end: "2026-03-31", year: "current" },
+  ];
+
+  // scales
+  const timeScale = getTimeScale(year, period).range([0, innerWidth]);
+
+  const displayMonths = months.filter((month) => {
+    const monthStart = getDateInUTC(month.begin);
+    const monthEnd = getDateInUTC(month.end);
+    const periodObj = periods.find((p) => p.value === period);
+    if (!periodObj) return false;
+
+    let periodStart = null;
+    let periodEnd = null;
+    if (year === "past") {
+      periodStart = getDateInUTC(periodObj.startPast.full);
+      periodEnd = getDateInUTC(periodObj.endPast.full);
+    } else {
+      periodStart = getDateInUTC(periodObj.startCurrent.full);
+      periodEnd = getDateInUTC(periodObj.endCurrent.full);
+    }
+
+    return (
+      monthEnd >= periodStart && monthStart <= periodEnd && year === month.year
+    );
+  });
+
+  console.log("Displaying months:", displayMonths);
 
   return html`<div>
     <svg
       viewBox="0 0 ${width} ${height}"
-      style="width: 100%; height: 100%; background-color: #f1f1f1"
+      style="width: 100%; height: 100%; border: 1px solid black;"
     >
       <g transform="translate(${margin.left},${margin.top})">
         <rect
@@ -401,6 +623,70 @@ function ComparisonChart({ data }) {
           height="${innerHeight}"
           fill="#f0f0f0"
         />
+        <circle
+          cx="${timeScale(getDateInUTC("2024-10-01"))}"
+          cy="50"
+          r="5"
+          fill="red"
+        />
+        <circle
+          cx="${timeScale(getDateInUTC("2024-11-01"))}"
+          cy="50"
+          r="5"
+          fill="orange"
+        />
+        <circle
+          cx="${timeScale(getDateInUTC("2024-12-01"))}"
+          cy="50"
+          r="5"
+          fill="orange"
+        />
+        <circle
+          cx="${timeScale(getDateInUTC("2025-01-01"))}"
+          cy="50"
+          r="5"
+          fill="orange"
+        />
+        <circle
+          cx="${timeScale(getDateInUTC("2025-02-01"))}"
+          cy="50"
+          r="5"
+          fill="orange"
+        />
+        <circle
+          cx="${timeScale(getDateInUTC("2025-03-01"))}"
+          cy="50"
+          r="5"
+          fill="orange"
+        />
+
+        <g>
+          ${displayMonths.map((month) => {
+            const xBegin = timeScale(getDateInUTC(month.begin)) || null;
+            const xEnd = timeScale(getDateInUTC(month.end)) || null;
+            if (xBegin === null || xEnd === null) return null;
+            return html`<g key=${month.name}>
+              <rect
+                x="${xBegin}"
+                y="${innerHeight + 20}"
+                width="${xEnd - xBegin}"
+                height="33"
+                fill="#f0f0f0"
+                fill-opacity="0.8"
+                rx="10"
+                ry="10"
+              />
+              <text
+                x="${(xBegin + xEnd) / 2}"
+                y="${innerHeight + 20 + 22}"
+                text-anchor="middle"
+                class="charts-text-body"
+              >
+                ${month.name}
+              </text>
+            </g>`;
+          })}
+        </g>
       </g>
     </svg>
   </div>`;
