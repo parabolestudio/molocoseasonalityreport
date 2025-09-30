93180689981fc68b447b3a6b937a80ce8974a520
Kristin Baumann <hello@kristin-baumann.com>
2025-09-30 13:32:07 +0200

    Fix loader file name

--- js/Loader.js
+++ js/Loader.js
@@ -4,6 +4,6 @@ import { ASSETS_URL } from "./helpers.js";
 export default function Loader({ isLoading, y }) {
   if (!isLoading) return null;
   return html`<div class="loader-overlay" style="top: ${y}px;">
-    <img src="${ASSETS_URL}/loader_100x.gif" alt="Loading..." />
+    <img src="${ASSETS_URL}/loader_100px.gif" alt="Loading..." />
   </div>`;
 }
