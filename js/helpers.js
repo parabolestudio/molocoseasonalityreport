79439cbb34b8354c0deb6d3113b2379ceba17406
Kristin Baumann <hello@kristin-baumann.com>
2025-09-30 15:00:27 +0200

    Rounding values on y axis

--- js/helpers.js
+++ js/helpers.js
@@ -4,38 +4,38 @@ export const ASSETS_URL =
 export const valueFormatting = {
   wau: (value) => {
     return value >= 1_000_000_000
-      ? (value / 1_000_000_000).toFixed(1).replace(/\.0$/, "") + "B"
+      ? (value / 1_000_000_000).toFixed(0).replace(/\.0$/, "") + "B"
       : value >= 1_000_000
-      ? (value / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M"
+      ? (value / 1_000_000).toFixed(0).replace(/\.0$/, "") + "M"
       : value >= 1_000
-      ? (value / 1_000).toFixed(1).replace(/\.0$/, "") + "k"
+      ? (value / 1_000).toFixed(0).replace(/\.0$/, "") + "k"
       : value;
   },
   downloads: (value) => {
     return value >= 1_000_000_000
-      ? (value / 1_000_000_000).toFixed(1).replace(/\.0$/, "") + "B"
+      ? (value / 1_000_000_000).toFixed(0).replace(/\.0$/, "") + "B"
       : value >= 1_000_000
-      ? (value / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M"
+      ? (value / 1_000_000).toFixed(0).replace(/\.0$/, "") + "M"
       : value >= 1_000
-      ? (value / 1_000).toFixed(1).replace(/\.0$/, "") + "k"
+      ? (value / 1_000).toFixed(0).replace(/\.0$/, "") + "k"
       : value;
   },
   revenue: (value) => {
     return value >= 1_000_000_000
-      ? (value / 1_000_000_000).toFixed(1).replace(/\.0$/, "") + "B"
+      ? (value / 1_000_000_000).toFixed(0).replace(/\.0$/, "") + "B"
       : value >= 1_000_000
-      ? (value / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M"
+      ? (value / 1_000_000).toFixed(0).replace(/\.0$/, "") + "M"
       : value >= 1_000
-      ? (value / 1_000).toFixed(1).replace(/\.0$/, "") + "k"
+      ? (value / 1_000).toFixed(0).replace(/\.0$/, "") + "k"
       : value;
   },
   time_spent: (value) => {
     return value >= 1_000_000_000
-      ? (value / 1_000_000_000).toFixed(1).replace(/\.0$/, "") + "B"
+      ? (value / 1_000_000_000).toFixed(0).replace(/\.0$/, "") + "B"
       : value >= 1_000_000
-      ? (value / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M"
+      ? (value / 1_000_000).toFixed(0).replace(/\.0$/, "") + "M"
       : value >= 1_000
-      ? (value / 1_000).toFixed(1).replace(/\.0$/, "") + "k"
+      ? (value / 1_000).toFixed(0).replace(/\.0$/, "") + "k"
       : value;
   },
 };
